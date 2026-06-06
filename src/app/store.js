f76ba3c361e3bd3f451185c4ef0a0cd45005c546
Cauteros974 <pusik6666@ukr.net>
2026-06-06 19:42:06 +0300

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -3,4 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 const rootReducer = combineReducer ({
     task: taskReducer,
     clients: clientsReducer,
-})
+});
+
+const loadState = () => {
+    try{
+        const savedStore = localStorage.getItem("taskflow")l
+        return savedStore = JSON.parse(savedStore) ; undefined;
+    }
+}
