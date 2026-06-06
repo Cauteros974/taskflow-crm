01fb389cd812d52f1af8b87dde8441c56f16aa85
Cauteros974 <pusik6666@ukr.net>
2026-06-06 19:38:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import App from "./App.jsx";
 import "antd/dist/reset.css";
 import "./styles/global.css";
 
+
+import { store } from "./app/store.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
