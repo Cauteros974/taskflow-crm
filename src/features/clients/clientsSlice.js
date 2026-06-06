4a7d7e9afb224ccb629470e889ede4983c1fc6c2
Cauteros974 <pusik6666@ukr.net>
2026-06-06 20:46:28 +0300

    Create clientsSlice.js

---
+++ src/features/clients/clientsSlice.js
@@ -0,0 +1,8 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { mockClients } from "../../data/mockData.js";
+
+const clientsSlice = createState({
+
+});
+
+export default clientsSlice;
