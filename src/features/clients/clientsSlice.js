0f98cba356fc08ace9ce461deecd68e93d8785f3
Cauteros974 <pusik6666@ukr.net>
2026-06-06 20:48:55 +0300

    Update clientsSlice.js

--- src/features/clients/clientsSlice.js
+++ src/features/clients/clientsSlice.js
@@ -2,7 +2,11 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { mockClients } from "../../data/mockData.js";
 
 const clientsSlice = createState({
+    name: "clients",
 
+    initialState: {
+        items: mockClients
+    }
 });
 
 export default clientsSlice;
