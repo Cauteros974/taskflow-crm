7ae104c9999e5a50d7d8713c414ab35c0b1850fd
Cauteros974 <pusik6666@ukr.net>
2026-06-06 19:54:34 +0300

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -3,3 +3,12 @@ export const status = ["New", "In process", "Under review", "Completed"];
 export const priorities = ["Low", "Middle", "Hight", "Critical"];
 
 export const projectStatues = ["Planning", "In Progress", "Pause", "Completed"];
+
+export const mockClients = [
+    {
+        id: "client-1",
+        name: "Anna Koval",
+        email: "anna.kovel@example.com",
+        phone: "+123 12 345 67 89",
+    }
+]
