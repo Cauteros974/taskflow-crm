3b8a4a1d34eb3a642694599d7df2174dd789ff78
Cauteros974 <pusik6666@ukr.net>
2026-06-06 19:52:09 +0300

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,3 +1,5 @@
 export const status = ["New", "In process", "Under review", "Completed"];
 
 export const priorities = ["Low", "Middle", "Hight", "Critical"];
+
+export const projectStatues = ["Planning", "In Progress", "Pause", "Completed"];
