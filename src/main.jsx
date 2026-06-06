84581fe6427e72686f25ee2dfcb65ff3ba49a8a7
Cauteros974 <pusik6666@ukr.net>
2026-06-06 19:37:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { ConfigProvider } from "antd";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ConfigProvider
+          theme={{
+            token: {
+              borderRadius: 10
+            }
+          }}
+        >
+          <CssBaseline />
+          <App />
+        </ConfigProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
