ba1e11e5fe2b3a24241fd0a03e44c9f520577591
Cauteros974 <pusik6666@ukr.net>
2026-06-09 19:11:03 +0300

    Update PageTitle.jsx

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -4,7 +4,9 @@ export default function PageTitle() {
     return(
         <Box className="page-profile">
             <Box>
-
+                <Typography variant="h4" fontWeight={900}>
+                    {title}
+                </Typography>
             </Box>
         </Box>
     )
