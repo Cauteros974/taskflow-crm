b2886ff14b9c573b4d2ea6f94884290805f15670
Cauteros974 <pusik6666@ukr.net>
2026-06-09 19:10:46 +0300

    Create PageTitle.jsx

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+export default function PageTitle() {
+    return(
+        <Box className="page-profile">
+            <Box>
+
+            </Box>
+        </Box>
+    )
+}
