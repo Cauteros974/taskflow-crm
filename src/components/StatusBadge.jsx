fa429ef4084cc10f424b662126e682ed16807b01
Cauteros974 <pusik6666@ukr.net>
2026-06-09 19:19:24 +0300

    Update StatusBadge.jsx

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -2,7 +2,10 @@ const colorMap = {
     New: "badge-blue",
     "In progress": "badge-orange",
     "Under review": "badge-purple",
-    Completed: "badge-green"
+    Completed: "badge-green",
+
+    Active: "badge-green",
+    Potential: "badge-blue",
 }
 
 export default function StatusMap({}) {
