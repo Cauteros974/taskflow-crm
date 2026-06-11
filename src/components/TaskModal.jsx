3de19f70a86cd2d22058df0f27dba504f07074d9
Cauteros974 <pusik6666@ukr.net>
2026-06-11 21:00:31 +0300

    Update TaskModal.jsx

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -11,12 +11,22 @@ import {
 import { DatePicker } from "antd";
 import dayjs from "dayjs";
 
+import HeadlessSelect from "./HeadlessSelect";
+import { status, priorities} from "../data/mockData";
+
 export default function TaskModal(){
     return(
         <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
             <DialogTitle>
                 {task? "Edit task": "New task"}
             </DialogTitle>
+
+            <DialogContent className="modal-content">
+                <TextField 
+                    label="Name"
+                    value={form.title}
+                />
+            </DialogContent>
         </Dialog>
     )
 }
