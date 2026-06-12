9b09038dc021f045b905f4aa5fd9862bf9da41c5
Cauteros974 <pusik6666@ukr.net>
2026-06-12 19:42:06 +0300

    Update Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import {
+  Card,
+  CardContent,
+  Grid,
+  LinearProgress,
+  Typography
+} from "@mui/material";
+import { Table } from "antd";
+
+import PageTitle from "../components/PageTitle.jsx";
+import StatusBadge from "../components/StatusBadge.jsx";
+
+export default function Dashboard() {
+    const tasks = useSelector((state) => state.tasks.items);
+    
+}
