3d17dd5e46b60357d1e3b9747274ba9fd508d7c9
Cauteros974 <pusik6666@ukr.net>
2026-06-12 19:48:11 +0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -39,7 +39,11 @@ export default function Dashboard() {
     const columns = [
         {
             title: "Task",
-            dataIndex: "Title"
+            dataIndex: "title"
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
         }
     ]
 }
