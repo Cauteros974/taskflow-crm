75d8534acec91c79621cc9b88c37375c719d54f8
Cauteros974 <pusik6666@ukr.net>
2026-06-13 20:27:26 +0300

    Update ProjectsPage.jsx

---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Button,
+  Card,
+  Grid,
+} from "@mui/material";
+
+import { Tab } from "@headlessui/react";
+import dayjs from "dayjs";
+import AddIcon from "@mui/icons-material/Add";
+
+import PageTitle from "../components/PageTitle";
+import StatusBadge from "../components/StatusBadge";
+
+import { addProject, deleteProject, updateProject } from "../features/projects/projectsSlice";
+
+export default function ProjectsPage() {
+    const dispatch = useDispatch();
+
+    const projects = useSelector(() => state.project.items);
+    const clients = useSelector(() => state.clients.items);
+}
