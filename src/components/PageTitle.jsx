c156748aec008e4cc88f695da58900e2e109d528
Cauteros974 <pusik6666@ukr.net>
2026-06-15 20:34:27 +0300

    Update PageTitle.jsx

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,21 +1,21 @@
 import { Box, Typography } from "@mui/material";
 
-export default function PageTitle({title, subtitle}) {
-    return(
-        <Box className="page-profile">
-            <Box>
-                <Typography variant="h4" fontWeight={900}>
-                    {title}
-                </Typography>
+export default function PageTitle({ title, subtitle, action }) {
+  return (
+    <Box className="page-title">
+      <Box>
+        <Typography variant="h4" fontWeight={900}>
+          {title}
+        </Typography>
 
-                {subtitle && (
-                    <Typography variant="body1" color="text.secondary">
-                        {subtitle}
-                    </Typography>
-                )}
-            </Box>
+        {subtitle && (
+          <Typography variant="body1" color="text.secondary">
+            {subtitle}
+          </Typography>
+        )}
+      </Box>
 
-            {action}
-        </Box>
-    );
+      {action}
+    </Box>
+  );
 }
