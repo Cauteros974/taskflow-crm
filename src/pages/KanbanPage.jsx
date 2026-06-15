c2cf6e54ac6c2ef502047c5620828423054b96e9
Cauteros974 <pusik6666@ukr.net>
2026-06-15 20:25:41 +0300

    Update KanbanPage.jsx

--- src/pages/KanbanPage.jsx
+++ src/pages/KanbanPage.jsx
@@ -108,11 +108,10 @@ function DraggableTask({task}) {
     ? { 
         transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` 
       } : undefined;
-}
 
     return(
         <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
             <TaskCard task={task} isDragging={isDragging} />
         </div>
-    )
+    );
 }
