afb191c5a848f6f787b6287ce7c568a9d15fa226
Cauteros974 <pusik6666@ukr.net>
2026-06-15 20:24:02 +0300

    Update KanbanPage.jsx

--- src/pages/KanbanPage.jsx
+++ src/pages/KanbanPage.jsx
@@ -104,9 +104,8 @@ function DraggableTask({task}) {
         useDraggable({
             id: task.id
         })
-
+    const style = transform ? { transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` } : undefined;
 }
-
 
     return(
         <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
