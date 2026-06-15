0b300677bb1bdcae0ff5a7aaf80efea323a04dd5
Cauteros974 <pusik6666@ukr.net>
2026-06-15 21:58:04 +0300

    Update HeadlessSelect.jsx

--- src/components/HeadlessSelect.jsx
+++ src/components/HeadlessSelect.jsx
@@ -1,37 +1,35 @@
 import { Listbox } from "@headlessui/react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-export default function HeadlessSelect({ value, onChange, options, label}) {
-    return (
-        <div className="select-wrapper">
-            {label && <span className="select-label">{label}</span>}
-            <Listbox value={value} onChange={onChange}>
-                <div className="listbox">
-                    <Listbox.Button className="listbox-button">
-                        {value}
-                        <KeyboardArrowDownIcon fontSize="small" />
-                    </Listbox.Button>
-                    <Listbox.Option className="listbox-options">
-                        {options.map((option) => (
-                            <Listbox.Option key={option} value={option}>
-                              {option.map((option)=>(
-                                <Listbox.Option key={option} value={option}>
-                                    {({ focus, selected }) => (
-                                        <div
-                                            className={`listbox-option ${
-                                                focus ? "focused" : ""
-                                        } ${selected ? "selected" : ""}`}
-                                        >
-                                            {option}
-                                        </div>
-                                    )}
-                                </Listbox.Option>
-                              ))}
-                            </Listbox.Option>
-                        ))}
-                    </Listbox.Option>
-                </div>
-            </Listbox>
+export default function HeadlessSelect({ value, onChange, options, label }) {
+  return (
+    <div className="select-wrapper">
+      {label && <span className="select-label">{label}</span>}
+
+      <Listbox value={value} onChange={onChange}>
+        <div className="listbox">
+          <Listbox.Button className="listbox-button">
+            {value}
+            <KeyboardArrowDownIcon fontSize="small" />
+          </Listbox.Button>
+
+          <Listbox.Options className="listbox-options">
+            {options.map((option) => (
+              <Listbox.Option key={option} value={option}>
+                {({ focus, selected }) => (
+                  <div
+                    className={`listbox-option ${
+                      focus ? "focused" : ""
+                    } ${selected ? "selected" : ""}`}
+                  >
+                    {option}
+                  </div>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
         </div>
-    );
+      </Listbox>
+    </div>
+  );
 }
