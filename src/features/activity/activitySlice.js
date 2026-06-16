e662c138ee988493aedc45aeeabc0947cece5454
Cauteros974 <pusik6666@ukr.net>
2026-06-16 20:27:40 +0300

    Update activitySlice.js

--- src/features/activity/activitySlice.js
+++ src/features/activity/activitySlice.js
@@ -3,13 +3,19 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 const initialState = {
     items: [
         {
-            id: "activity",
+            id: "activity-1",
             type: "system",
             title: "The project has been launched",
             entityType: "system",
             description: "TaskFlow CRM is ready to go.",
             entityId: null,
             createdAt: "2026-06-05 10:00"
-        }
+        },
     ]
-}
+};
+
+const activitySlice = createSlice({
+    name="activity",
+
+    initialState
+})
