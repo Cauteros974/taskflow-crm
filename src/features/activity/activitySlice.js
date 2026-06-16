50c7353fc7ddd9cc62875d75f1a575a6da29b7e7
Cauteros974 <pusik6666@ukr.net>
2026-06-16 20:26:26 +0300

    Update activitySlice.js

--- src/features/activity/activitySlice.js
+++ src/features/activity/activitySlice.js
@@ -8,7 +8,8 @@ const initialState = {
             title: "The project has been launched",
             entityType: "system",
             description: "TaskFlow CRM is ready to go.",
-            entityId: null
+            entityId: null,
+            createdAt: "2026-06-05 10:00"
         }
     ]
 }
