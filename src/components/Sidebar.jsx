d2fe2eb464f145f004d4b45b6af50283fdc110ee
Cauteros974 <pusik6666@ukr.net>
2026-06-19 21:05:19 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import TaskIcon from "@mui/icons-material/Task";
 import GroupsIcon from "@mui/icons-material/Groups";
 import FolderIcon from "@mui/icons-material/Folder";
 import ViewKanbanIcon from "@mui/icons-material/ViewKanban";
+import HistoryIcon from "@mui/icons-material/History";
 
 const navItems = [
   {
@@ -32,31 +33,36 @@ const navItems = [
     to: "/kanban",
     label: "Kanban",
     icon: <ViewKanbanIcon />
+  },
+  {
+    to: "/activity",
+    label: "History",
+    icon: <HistoryIcon />
   }
 ];
 
 export default function Sidebar() {
-    return(
-        <Box clasname = "sidebar">
-            <Typography variant="h6" fontWeight={900} className="logo">
-                TaskFlow
-            </Typography>
+  return (
+    <Box className="sidebar">
+      <Typography variant="h6" fontWeight={900} className="logo">
+        TaskFlow
+      </Typography>
 
-            <nav className="nav">
-                {navItems.map((item) => (
-                    <NavLink
-                        key={item.to}
-                        to={item.to}
-                        end={item.to === "/"}
-                        className={({ isActive }) =>
-                            isActive ? "nav-link active" : "nav-link"
-                        }
-                    >
-                        {item.icon}
-                        <span>{item.label}</span>
-                    </NavLink>
-                ))}
-            </nav>
-        </Box>
-    );
+      <nav className="nav">
+        {navItems.map((item) => (
+          <NavLink
+            key={item.to}
+            to={item.to}
+            end={item.to === "/"}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            {item.icon}
+            <span>{item.label}</span>
+          </NavLink>
+        ))}
+      </nav>
+    </Box>
+  );
 }
