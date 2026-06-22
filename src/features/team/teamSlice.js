ca5cbc74dd333bf6ccd55630043cfca3cee1802c
Cauteros974 <pusik6666@ukr.net>
2026-06-22 21:42:21 +0300

    Update teamSlice.js

--- src/features/team/teamSlice.js
+++ src/features/team/teamSlice.js
@@ -1,58 +1,56 @@
-import { nanoid, createSlice } from "@reduxjs/toolkit";
-import { mockTeam } from "../../data/mockData";
-import { addTask } from "../tasks/tasksSlice";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { mockTeam } from "../../data/mockData.js";
 
 const teamSlice = createSlice({
+  name: "team",
 
-    name: "team",
+  initialState: {
+    items: mockTeam
+  },
 
-    initialState: {
-        items: mockTeam
+  reducers: {
+    addTeamMember: {
+      reducer(state, action) {
+        state.items.push(action.payload);
+      },
+
+      prepare(member) {
+        return {
+          payload: {
+            id: nanoid(),
+            ...member,
+            capacity: Number(member.capacity)
+          }
+        };
+      }
     },
 
-    reducers: {
-        addTeamMember: {
-            reducer (state, action) {
-                state.items.push(action.payload)
-            },
+    updateTeamMember(state, action) {
+      const index = state.items.findIndex(
+        (member) => member.id === action.payload.id
+      );
 
-            prepare(team) {
-                return{
-                    payload: {
-                        id: nanoid(),
-                        ...member,
-                        capacity: Number(member.capacity)
-                    }
-                };
-            }
-        },
+      if (index !== -1) {
+        state.items[index] = {
+          ...state.items[index],
+          ...action.payload,
+          capacity: Number(action.payload.capacity)
+        };
+      }
+    },
 
-        updateTeamMemeber(state, action) {
-            const index = state.items.findIndex(
-                (member) => member.id === action.payload.id
-            );
-
-            if(index !== 1) {
-                state.items[index] = {
-                    ...state.items[index],
-                    ...action.payload,
-                    capacity: Number(action.payload.capacity)
-                };
-            }
-        },
-
-        deleteTeamMember(state, action) {
-            state.items = state.items.filter(
-                (member) => member.id !== action.payload
-            );
-        }
+    deleteTeamMember(state, action) {
+      state.items = state.items.filter(
+        (member) => member.id !== action.payload
+      );
     }
+  }
 });
 
 export const {
-    addTeamMember,
-    updateTeamMember,
-    deleteTeamMember
+  addTeamMember,
+  updateTeamMember,
+  deleteTeamMember
 } = teamSlice.actions;
 
 export default teamSlice.reducer;
